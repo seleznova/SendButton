e0e534a0d8df66c4ca5b04732301f1447d637dd8
seleznova <mary.kuzmenko@gmail.com>
2017-11-14 18:52:58 +0000

    Final improvements

--- app/src/main/java/marynakuzmenko/sendbutton/MainActivity.java
+++ app/src/main/java/marynakuzmenko/sendbutton/MainActivity.java
@@ -16,7 +16,7 @@ public class MainActivity extends AppCompatActivity {
 
     /** Called when the user taps the Send button */
     public void sendMessage(View view) {
-        Intent intent = new Intent(this, DisplayMessageActivity.class);
+        Intent intent = new Intent(this, DisplayMessageActivity2.class);
         EditText editText = (EditText) findViewById(R.id.editText);
         String message = editText.getText().toString();
         intent.putExtra(EXTRA_MESSAGE, message);
